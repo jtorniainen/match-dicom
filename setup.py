61f026c0103d3c761baffbc0cb6df402819a1d50
jtorniainen <jtorniainen@users.noreply.github.com>
2017-03-16 08:01:53 +0200

    renaming because setup dislikes current naming scheme

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(name='match-dicom',
       version='0.1',
       description='Simple, command-line DICOM-RAW matcher',
       author='jtorniainen, Biophysics of Bone and Cartilage, Department of Applied Physics, University of Eastern Finland',
-      url='https://github.com/jtorniainen/match-dicom',
+      url='https://github.com/jtorniainen/matchdicom',
       license='MIT',
-      packages=['match-dicom'],
-      package_dir={'match-dicom': 'match-dicom'},
+      packages=['matchdicom'],
+      package_dir={'matchdicom': 'matchdicom'},
       include_package_data=False,
       install_requires=['blessings>=1.6',
                         'pydicom>=0.9.9',
                         'tifffile>=0.10.0'],
       entry_points={"console_scripts":
-                    ["mdicom = match-dicom.match-dicom:run_from_cli"]})
+                    ["mdicom = matchdicom.matchdicom:run_from_cli"]})
